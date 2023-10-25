ffc7ea6a2947e9c2d33bd6283ca0baa60c0c47b8
hd2g <nagisa.159s.8673.trap@gmail.com>
2023-10-25 17:09:59 +0900

    [count-changes-frontend] WIP: coding...

--- count-changes-frontend/main.rkt
+++ count-changes-frontend/main.rkt
@@ -1,20 +1,72 @@
 #lang racket
 
+(provide (all-defined-out))
+
 (require web-server/servlet
          web-server/servlet-env)
 
-(define (app request)
-  (response/xexpr
-   `(html
-     (head
-      (meta ((charset "utf-8")))
-      (meta ((name "viewport")
-             (content "width=device-width,initial-scale=1")))
-      (title "Count Changes"))
-     (body
-      (h1 "Count Changes")))))
+(define app-name "Count Changes")
 
-(serve/servlet app
-               #:launch-browser? #f
-               #:port 3000
-               #:servlet-path "/")
+(define (header title href)
+  `(div
+    (a ((alt ,app-name) (href ,href))
+      (h1 ,app-name))))
+
+(define give-an-amount
+  `(div
+    (label
+     "How match?"
+     (input ((id "amount") (type "number"))))))
+
+; (define (give-an-amount amount)
+;   (call/cc
+;    (lambda (k)
+;      (let go ()
+;        (k
+;         (lambda (embed/url)
+;           (let ((
+;           `(div
+;             (label
+;              "How match?"
+;              (input ((id "amount")
+;                      (type "number"))))))
+
+(define contents
+  `(main
+    ,(header app-name "/")
+    ,give-an-amount))
+
+; (define (app request)
+;   (define amount (make-web-cell 0))
+
+;   (send/suspend/dispatch
+;    (lambda (embed/url)
+;      (response/xexpr
+;       `(html
+;         (head
+;          (meta ((charset "utf-8")))
+;          (meta ((name "viewport")
+;                 (content "width=device-width,initial-scale=1")))
+;          (title ,app-name))
+;         (body
+;          ,contents))))))
+
+(define (app request)
+   (response/xexpr
+    `(html
+      (head
+       (meta ((charset "utf-8")))
+       (meta ((name "viewport")
+              (content "width=device-width,initial-scale=1")))
+       (title ,app-name))
+      (body
+       ,contents))))
+
+(define (main)
+  (serve/servlet app
+                 #:launch-browser? #f
+                 #:port 3000
+                 #:servlet-path "/"))
+
+(module+ main
+  (main))
